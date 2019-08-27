d908f17ca4b9d23a7307b62ad269c324faf3264f
Afsaneh Sadeghnia <x.sadeghnia@gmail.com>
2019-08-27 22:25:55 +0200

    Refactor endpoint class.

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -22,9 +22,6 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint("/chat")
 public class WebSocketChatServer {
 
-    /**
-     * All chat sessions.
-     */
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
     private static Map<String, String> sessionIdToUsernameMap = new ConcurrentHashMap<>();
 
@@ -32,20 +29,16 @@ public class WebSocketChatServer {
        return onlineSessions.containsKey(userName);
     }
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     public WebSocketChatServer() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                ObjectMapper objectMapper = new ObjectMapper();
                 while (true){
                     try {
                         ChatResponse chatResponse = ChatResponse.createUserCountResponse(onlineSessions.values().size());
-                        String responseStr = objectMapper.writeValueAsString(chatResponse);
-                        for (Session s : onlineSessions.values()){
-                            s.getBasicRemote().sendText(responseStr);
-                        }
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
+                        sendToAll(chatResponse);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -59,107 +52,66 @@ public class WebSocketChatServer {
         }).start();
     }
 
-    /**
-     * Open connection, 1) add session, 2) add user.
-     */
     @OnOpen
     public void onOpen(Session session) {
-
     }
 
-    /**
-     * Send message, 1) get username and session, 2) send message to all.
-     */
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
-
-        ObjectMapper objectMapper = new ObjectMapper();
         try {
             // Deserialize request json string to request object.
             ChatRequest chatRequest = objectMapper.readValue(jsonStr, ChatRequest.class);
             switch (chatRequest.getType()){
                 case ChatRequest.ENTER: {
-                    // Update internal maps.
+
                     String username = chatRequest.getArg1();
                     onlineSessions.put(username, session);
                     sessionIdToUsernameMap.put(session.getId(), username);
-                    // Create response object.
-                    ChatResponse welcomeResponse = ChatResponse.createWelcomeResponse(username);
-                    // Serialize response object to json.
-                    String welcomeStr = objectMapper.writeValueAsString(welcomeResponse);
-                    // Send response json string to client.
-                    session.getBasicRemote().sendText(welcomeStr);
 
-                    // Create USER_JOINED response object.
+                    ChatResponse welcomeResponse = ChatResponse.createWelcomeResponse(username);
+                    sendToSession(session, welcomeResponse);
+
                     ChatResponse joinedResponse = ChatResponse.createUserJoinedResponse(username);
-                    String joinedStr = objectMapper.writeValueAsString(joinedResponse);
-                    for (Session s : onlineSessions.values()){
-                        s.getBasicRemote().sendText(joinedStr);
-                    }
+                    sendToAll(joinedResponse);
+
                     ChatResponse listResponse = ChatResponse.createUserListResponse(
                             new ArrayList<>(onlineSessions.keySet()));
-                    String listStr = objectMapper.writeValueAsString(listResponse);
-                    session.getBasicRemote().sendText(listStr);
+                    sendToSession(session,listResponse );
+
                     break;
                 }
                 case ChatRequest.CHAT: {
-                    // Create response object.
                     ChatResponse receivedResponse = ChatResponse.createReceivedResponse();
-                     // Serialize.
-                    String responseStr = objectMapper.writeValueAsString(receivedResponse);
-                    // Send response json string to client.
-                    session.getBasicRemote().sendText(responseStr);
-                    //
+                    sendToSession(session, receivedResponse);
+
                     String username = sessionIdToUsernameMap.get(session.getId());
                     String message = chatRequest.getArg1();
                     ChatResponse notifyResponse = ChatResponse.createNotifyResponse(username, message);
-                    // Serialize notify message.
-                    String notifyStr = objectMapper.writeValueAsString(notifyResponse);
-                    // Send to all sessions
-                    for (Session s : onlineSessions.values()){
-                        s.getBasicRemote().sendText(notifyStr);
-                    }
+                    sendToAll(notifyResponse);
 
                     break;
                 }
                 case ChatRequest.DIRECT: {
-
                     ChatResponse receivedResponse = ChatResponse.createReceivedResponse();
-                    String responseStr = objectMapper.writeValueAsString(receivedResponse);
-                    session.getBasicRemote().sendText(responseStr);
-                    //
+                    sendToSession(session, receivedResponse);
+
                     String username = sessionIdToUsernameMap.get(session.getId());
                     String message = chatRequest.getArg1();
                     ChatResponse notifyResponse = ChatResponse.createNotifyResponse(username, message);
-                    // Serialize
-                    String notifyStr = objectMapper.writeValueAsString(notifyResponse);
-                    // send to a person.
                     Session receiverSession = onlineSessions.get(chatRequest.getArg2());
-                    for (Session s:onlineSessions.values()){
-                        if (s == receiverSession){
-                            s.getBasicRemote().sendText(notifyStr);
-                        }
-                    }
+                    sendToSession(receiverSession,notifyResponse);
+
                     break;
                 }
                 case ChatRequest.LEAVE: {
-                    // Leave logic
-                    String username = sessionIdToUsernameMap.get(session.getId());
-                    onlineSessions.remove(username);
-                    sessionIdToUsernameMap.remove(session.getId());
+                    removeUser(session);
 
-                    // Create response object.
                     ChatResponse byeResponse = ChatResponse.createByeResponse();
-                    // Serialize
-                    String responseStr = objectMapper.writeValueAsString(byeResponse);
-                    // Send response json string to client.
-                    session.getBasicRemote().sendText(responseStr);
+                    sendToSession(session, byeResponse);
 
+                    String username = sessionIdToUsernameMap.get(session.getId());
                     ChatResponse userLeft = ChatResponse.createUserLeftResponse(username);
-                    String leftStr = objectMapper.writeValueAsString(userLeft);
-                    for (Session s : onlineSessions.values()) {
-                        s.getBasicRemote().sendText(leftStr);
-                    }
+                    sendToAll(userLeft);
 
                     break;
                 }
@@ -172,28 +124,30 @@ public class WebSocketChatServer {
         }
     }
 
-    /**
-     * Close connection, 1) remove session, 2) update user.
-     */
     @OnClose
     public void onClose(Session session) {
-        String username = sessionIdToUsernameMap.get(session.getId());
-        onlineSessions.remove(username);
-        sessionIdToUsernameMap.remove(session.getId());
-        System.out.println("close!");
-
+        removeUser(session);
     }
 
-    /**
-     * Print exception.
-     */
     @OnError
     public void onError(Session session, Throwable error) {
         error.printStackTrace();
+        removeUser(session);
+    }
+
+    private void sendToSession(Session session, ChatResponse chatResponse) throws IOException {
+        String responseStr = objectMapper.writeValueAsString(chatResponse);
+        session.getBasicRemote().sendText(responseStr);
+    }
+
+    private void sendToAll(ChatResponse chatResponse) throws IOException {
+        for (Session s:onlineSessions.values()){
+            sendToSession(s,chatResponse);
+        }
+    }
+    private  void removeUser(Session session){
         String username = sessionIdToUsernameMap.get(session.getId());
         onlineSessions.remove(username);
         sessionIdToUsernameMap.remove(session.getId());
-        System.out.println("error!");
     }
-
 }
