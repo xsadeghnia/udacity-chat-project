cbf5a8abee11b256f6fd02056693daf4ad576ade
Afsaneh Sadeghnia <x.sadeghnia@gmail.com>
2019-08-29 08:49:56 +0200

    Add unit tests.

--- src/test/java/edu/udacity/java/nano/chat/ChatServiceTest.java
+++ src/test/java/edu/udacity/java/nano/chat/ChatServiceTest.java
@@ -17,7 +17,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ChatServiceTest {
 
     @Test
-    public void testEnterRequest() throws IOException {
+    public void testWelcomeResponse() throws IOException {
 
         final AtomicBoolean fired = new AtomicBoolean(false);
         ChatEndpoint fakeChatEndpoint = new ChatEndpoint() {
@@ -62,5 +62,187 @@ public class ChatServiceTest {
         ChatService chatService = new ChatService(fakeChatEndpoint);
         chatService.handleEnterReq(chatRequest,null);
         Assert.assertTrue(fired.get());
+    }
+
+    @Test
+    public void testReceivedResponse() throws IOException {
+
+        final AtomicBoolean receivedResponseSeen = new AtomicBoolean(false);
+        ChatEndpoint fakeChatEndpoint = new ChatEndpoint() {
+            @Override
+            public void send(String username, ChatResponse chatResponse) throws IOException {
+                if (chatResponse.getType().equals(ChatResponse.RECEIVED)){
+                    receivedResponseSeen.set(true);
+                }
+            }
+
+            @Override
+            public void sendToAll(ChatResponse chatResponse) throws IOException {
+
+            }
+
+            @Override
+            public void registerUsername(String username, Session session) {
+
+            }
+
+            @Override
+            public void unregisterUsername(String username) {
+
+            }
+
+            @Override
+            public int getNumOfUsers() {
+                return 0;
+            }
+
+            @Override
+            public List<String> getAllUsernames() {
+                return null;
+            }
+        };
+
+        ChatRequest chatRequest = new ChatRequest();
+        chatRequest.setType(ChatRequest.CHAT);
+        chatRequest.setArg1("Hello!");
+        ChatService chatService = new ChatService(fakeChatEndpoint);
+        chatService.handleChatRequest(chatRequest, null);
+        Assert.assertTrue(receivedResponseSeen.get());
+    }
+
+    @Test
+    public void testNotifyResponse() throws IOException {
+
+        final AtomicBoolean notifyResponseSeen = new AtomicBoolean(false);
+        ChatEndpoint fakeChatEndpoint = new ChatEndpoint() {
+            @Override
+            public void send(String username, ChatResponse chatResponse) throws IOException {
+            }
+
+            @Override
+            public void sendToAll(ChatResponse chatResponse) throws IOException {
+                if (chatResponse.getType().equals(ChatResponse.NOTIFY)){
+                    Assert.assertEquals("Hello!" , chatResponse.getArg2());
+                    notifyResponseSeen.set(true);
+                }
+            }
+
+            @Override
+            public void registerUsername(String username, Session session) {
+
+            }
+
+            @Override
+            public void unregisterUsername(String username) {
+
+            }
+
+            @Override
+            public int getNumOfUsers() {
+                return 0;
+            }
+
+            @Override
+            public List<String> getAllUsernames() {
+                return null;
+            }
+        };
+
+        ChatRequest chatRequest = new ChatRequest();
+        chatRequest.setType(ChatRequest.CHAT);
+        chatRequest.setArg1("Hello!");
+        ChatService chatService = new ChatService(fakeChatEndpoint);
+        chatService.handleChatRequest(chatRequest, null);
+        Assert.assertTrue(notifyResponseSeen.get());
+    }
+
+    @Test
+    public void testByeResponse() throws IOException {
+
+        final AtomicBoolean byeResponseSeen = new AtomicBoolean(false);
+        ChatEndpoint fakeChatEndpoint = new ChatEndpoint() {
+            @Override
+            public void send(String username, ChatResponse chatResponse) throws IOException {
+                if (chatResponse.getType().equals(ChatResponse.BYE)){
+                    byeResponseSeen.set(true);
+                }
+            }
+
+            @Override
+            public void sendToAll(ChatResponse chatResponse) throws IOException {
+
+            }
+
+            @Override
+            public void registerUsername(String username, Session session) {
+
+            }
+
+            @Override
+            public void unregisterUsername(String username) {
+
+            }
+
+            @Override
+            public int getNumOfUsers() {
+                return 0;
+            }
+
+            @Override
+            public List<String> getAllUsernames() {
+                return null;
+            }
+        };
+
+        ChatRequest chatRequest = new ChatRequest();
+        chatRequest.setType(ChatRequest.LEAVE);
+        ChatService chatService = new ChatService(fakeChatEndpoint);
+        chatService.handleLeaveRequest(null);
+        Assert.assertTrue(byeResponseSeen.get());
+    }
+
+    @Test
+    public void testUserleftResponse() throws IOException {
+
+        final AtomicBoolean leftResponseSeen = new AtomicBoolean(false);
+        ChatEndpoint fakeChatEndpoint = new ChatEndpoint() {
+            @Override
+            public void send(String username, ChatResponse chatResponse) throws IOException {
+            }
+
+            @Override
+            public void sendToAll(ChatResponse chatResponse) throws IOException {
+                if (chatResponse.getType().equals(ChatResponse.USER_LEFT)){
+                    Assert.assertEquals("foo" , chatResponse.getArg1());
+                    leftResponseSeen.set(true);
+                }
+            }
+
+            @Override
+            public void registerUsername(String username, Session session) {
+
+            }
+
+            @Override
+            public void unregisterUsername(String username) {
+
+            }
+
+            @Override
+            public int getNumOfUsers() {
+                return 0;
+            }
+
+            @Override
+            public List<String> getAllUsernames() {
+                return null;
+            }
+        };
+
+        ChatRequest chatRequest = new ChatRequest();
+        chatRequest.setType(ChatRequest.LEAVE);
+        ChatService chatService = new ChatService(fakeChatEndpoint);
+        chatService.handleLeaveRequest("foo");
+        Assert.assertTrue(leftResponseSeen.get());
     }
 }
